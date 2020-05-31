ecaeda97506ee1a5deb64fc63671b4895524829d
Jaakko Leskelä <jaska.1999@hotmail.com>
2020-05-31 18:24:11 +0300

    Mainmenu scene

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Prefab;
+    public float SpawnInterval = 5f;
+    public float CurrentTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentTime += Time.deltaTime;
+        if(CurrentTime > SpawnInterval)
+        {
+            CurrentTime = 0;
+            
+        }
+    }
+}
